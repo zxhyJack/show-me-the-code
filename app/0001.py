003f2d3ddaea1515cf33998be8fc7cc2f4efced9
朱洪杰 <zhuhongjie0318@163.com>
2019-01-22 20:13:00 +0800

    0001

--- app/0001.py
+++ app/0001.py
@@ -1,12 +1,12 @@
 from PIL import Image, ImageDraw, ImageFont
 
 def add_num(path):
-    img = Image.open(path)
+    img = Image.open(path).convert('RGB')
     draw = ImageDraw.Draw(img)
-    myfont = ImageFont.truetype('C:/windows/fonts/Arial.ttf', size=40)
+    myfont = ImageFont.truetype('/usr/share/fonts/truetype/ubuntu-font-family/UbuntuMono-R.ttf', size=40)
     fillcolor = "#ff0000"
     width, height = img.size
-    draw.text((width-40, 0), '99', font=myfont, fill=fillcolor)
+    draw.text((width-30, 0), '3', font=myfont, fill=fillcolor)
     img.save('result.jpg','jpeg')
 
     return 0
